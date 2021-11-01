fb8d5d906841d95f72018bca672ac37834460804
vinny tsai <away0102@gmail.com>
2021-11-01 19:31:29 +0800

    add: finish pets detail page and layout

--- public/js/detail.js
+++ public/js/detail.js
@@ -3,13 +3,15 @@ const currentUrl = location.href;
 const queryString = currentUrl.split("?id=");
 const id = queryString[1];
 const token = localStorage.getItem("access_token");
+const person = location.href.split("/f")[1].split(".")[0].split("/")[0];
+console.log(person);
 
 getPostDetail();
 
 // render by id
 async function getPostDetail() {
   try {
-    const fetchData = await fetch(`/api/findowners/detail?id=${id}`, {
+    const fetchData = await fetch(`/api/f${person}/detail?id=${id}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -21,18 +23,18 @@ async function getPostDetail() {
     console.log(data);
     // check if user login or not
     if (data.roomId) {
-      // element with roomid
-      const href = `/chatroom.html?id=${data.ownerId}&room=${data.roomId}`;
-      createElement(data, href);
-    } else {
       if (data.roomId == "null") {
         // element with self post
         createElementSelfPost(data);
       } else {
-        // element with redirect href
-        const href = `/chatroom.html`;
-        createElement(data, href);
+        // element with roomid
+        const roomIdHref = `/chatroom.html?room=${data.roomId}`;
+        createElement(data, roomIdHref);
       }
+    } else {
+      // element with redirect href
+      const href = `/chatroom.html`;
+      createElement(data, href);
     }
   } catch (err) {
     console.log(err);
@@ -41,43 +43,55 @@ async function getPostDetail() {
 
 function createElement(data, href) {
   const div = document.createElement("div");
+  div.className = "photo-info-wrap";
   const parentDiv = document.querySelector(".main-wrap");
-  div.innerHTML = ` <div class="photo">
-  <img src="${data.photo}"/>
-</div>
-<div class="info">
-  <div>品種 : ${data.breed}</div>
-  <div>地點 : ${data.address}</div>
-  <div>時間 : ${data.date}</div>
-  <div>備註 : ${data.note}</div>
-
-  <div class="finderinfo">
-    <div><img src="${data.picture}"/></div>
-    <div>Finder Name: ${data.ownername}</div>
-  </div>
-  <div class=>
-    <a href="${href}">chat with me</a>
-    <a href="/">This is my pet</a>
-  </div>
-</div>`;
+  div.innerHTML = `
+    <div class="photo-wrap">
+      <div class="photo">
+        <img src="${data.photo}"/>
+      </div>
+      <div><a href="/">這是我的寵物</a></div>
+    </div>
+    <div class="info-wrap">
+      <div>
+        <div class="info"><span>品種 : </span>${data.breed}</div>
+        <div class="info"><span>顏色 : </span>${data.color}</div>
+        <div class="info"><span>地點 : </span>${data.address}</div>
+        <div class="info"><span>時間 : </span>${data.date}</div>
+        <div class="info"><span>備註 : </span>${data.note}</div>
+      </div>
+      <div class="finderinfo">
+        <div><img src="${data.postUserPic}"/></div>
+        <div class="finder-name"><span>Finder Name: </span>${data.postUserName}</div>
+        <div class=chat-button>
+          <a href="${href}">傳送訊息</a>
+        </div>
+      </div>
+    </div>`;
   parentDiv.appendChild(div);
 }
 
 function createElementSelfPost(data) {
   const div = document.createElement("div");
+  div.className = "photo-info-wrap";
   const parentDiv = document.querySelector(".main-wrap");
-  div.innerHTML = ` <div class="photo">
-  <img src="${data.photo}"/>
+  div.innerHTML = `
+  <div class="photo-wrap">
+  <div class="photo">
+    <img src="${data.photo}"/>
+  </div>
 </div>
-<div class="info">
-  <div>品種 : ${data.breed}</div>
-  <div>地點 : ${data.address}</div>
-  <div>時間 : ${data.date}</div>
-  <div>備註 : ${data.note}</div>
-
+<div class="info-wrap">
+  <div>
+    <div class="info"><span>品種 : </span>${data.breed}</div>
+    <div class="info"><span>顏色 : </span>${data.color}</div>
+    <div class="info"><span>地點 : </span>${data.address}</div>
+    <div class="info"><span>時間 : </span>${data.date}</div>
+    <div class="info"><span>備註 : </span>${data.note}</div>
+  </div>
   <div class="finderinfo">
-    <div><img src="${data.picture}"/></div>
-    <div>Finder Name: ${data.ownername}</div>
+    <div><img src="${data.postUserPic}"/></div>
+    <div class="finder-name"><span>Finder Name: </span>${data.postUserName}</div>
   </div>
 </div>`;
   parentDiv.appendChild(div);
