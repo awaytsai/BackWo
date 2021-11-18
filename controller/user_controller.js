0df8483c6cf2242cb801b80d66c43f478b594c53
vinny tsai <away0102@gmail.com>
2021-11-18 19:19:11 +0800

    update: add race condition in signup signin

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -4,10 +4,12 @@ const Auth = require("../util/auth");
 const crypto = require("crypto");
 
 const signUp = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
+  let { name } = req.body;
+  name = validator.escape(name);
   const provider = "native";
   const picture = `${process.env.CLOUDFRONT}/member_default_image.png`;
-  // const picture = `https://cdn2.iconfinder.com/data/icons/veterinary-solid/96/paw_vet_pet_animal-512.png`;
+
   if (!name || !email || !password) {
     res.status(400).json({ error: "請輸入名字、email 和密碼" });
     return;
@@ -19,74 +21,97 @@ const signUp = async (req, res) => {
   if (password.length < 6) {
     res.status(400).json({ error: "請輸入至少6位數密碼" });
   }
-  // check existing email
-  const checkExistEmail = await User.checkExistedEmail(email);
-  if (checkExistEmail[0].count > 0) {
-    res.status(400).json({ error: "email 已經被註冊過，請更換" });
+  //////// race condition
+  const result = await User.signUp(provider, name, email, password, picture);
+  if (result.error) {
+    res.status(403).json({ error: result.error });
     return;
   }
-  // hash password
-  const hashPassword = crypto
-    .createHash("sha256")
-    .update(password)
-    .digest("hex");
-  // insert user data
-  const userData = await User.insertUserData(
-    provider,
-    name,
-    email,
-    hashPassword,
-    picture
-  );
-  const userId = userData.insertId;
-  // console.log(userId);
-  // format response
-  const payload = {
-    id: userId,
-    name: name,
-    email: email,
-    provider: provider,
-    picture: picture,
-  };
-  const token = await Auth.jwtTokenGenerater(payload);
-  const userResult = Auth.userinfoFormat(token, payload);
-  // console.log(userResult);
-  res.status(200).json(userResult);
+  return res.status(200).json(result);
+  ////////
+  // // check existing email
+  // const checkExistEmail = await User.checkExistedEmail(email);
+  // if (checkExistEmail[0].count > 0) {
+  //   res.status(400).json({ error: "email 已經被註冊過，請更換" });
+  //   return;
+  // }
+  // // hash password
+  // const hashPassword = crypto
+  //   .createHash("sha256")
+  //   .update(password)
+  //   .digest("hex");
+  // // insert user data
+  // const userData = await User.insertUserData(
+  //   provider,
+  //   name,
+  //   email,
+  //   hashPassword,
+  //   picture
+  // );
+  // const userId = userData.insertId;
+  // // console.log(userId);
+  // // format response
+  // const payload = {
+  //   id: userId,
+  //   name: name,
+  //   email: email,
+  //   provider: provider,
+  //   picture: picture,
+  // };
+  // const token = await Auth.jwtTokenGenerater(payload);
+  // const userResult = Auth.userinfoFormat(token, payload);
+  // // console.log(userResult);
+  // res.status(200).json(userResult);
 };
 
-//
 const signIn = async (req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
     res.status(400).json({ error: "請輸入 email 和密碼" });
     return;
   }
-  const userData = await User.getUserDataByEmail(email);
-  if (userData.length == 0) {
-    res.status(400).json({ error: "email 不存在" });
-    return;
+  try {
+    const result = await User.nativeSignIn(email, password);
+    console.log(result);
+    return res.status(200).json(result);
+  } catch (error) {
+    return { error };
   }
-  const hashPassword = crypto
-    .createHash("sha256")
-    .update(password)
-    .digest("hex");
-
-  if (userData[0].password !== hashPassword) {
-    res.status(400).json({ error: "密碼錯誤" });
-    return;
-  }
-  // format response
-  const payload = {
-    id: userData[0].id,
-    name: userData[0].name,
-    email: userData[0].email,
-    provider: userData[0].provider,
-    picture: userData[0].picture,
-  };
-  const token = await Auth.jwtTokenGenerater(payload);
-  const userResult = Auth.userinfoFormat(token, payload);
-  res.status(200).json(userResult);
 };
+
+// const signIn = async (req, res) => {
+//   const { email, password } = req.body;
+//   if (!email || !password) {
+//     res.status(400).json({ error: "請輸入 email 和密碼" });
+//     return;
+//   }
+//   const userData = await User.getUserDataByEmail(email);
+//   if (userData.length == 0) {
+//     res.status(400).json({ error: "email 不存在" });
+//     return;
+//   }
+//   const hashPassword = crypto
+//     .createHash("sha256")
+//     .update(password)
+//     .digest("hex");
+
+//   if (userData[0].password !== hashPassword) {
+//     res.status(400).json({ error: "密碼錯誤" });
+//     return;
+//   }
+//   // format response
+//   const payload = {
+//     id: userData[0].id,
+//     name: userData[0].name,
+//     email: userData[0].email,
+//     provider: userData[0].provider,
+//     picture: userData[0].picture,
+//   };
+//   const token = await Auth.jwtTokenGenerater(payload);
+//   const userResult = Auth.userinfoFormat(token, payload);
+//   res.status(200).json(userResult);
+// };
 
 const getUserData = async (req, res) => {
   const payload = req.decoded.payload;
--- model/user_model.js
+++ model/user_model.js
@@ -1,20 +1,22 @@
 const db = require("../db");
+const crypto = require("crypto");
+const { jwtTokenGenerater, userinfoFormat } = require("../util/auth");
 
-const checkExistedEmail = async (email) => {
-  const [existingEmail] = await db.query(
-    "SELECT count(email) as count FROM user WHERE email = ? ;",
-    [email]
-  );
-  return existingEmail;
-};
+// const checkExistedEmail = async (email) => {
+//   const [existingEmail] = await db.query(
+//     "SELECT count(email) as count FROM user WHERE email = ? ;",
+//     [email]
+//   );
+//   return existingEmail;
+// };
 
-const insertUserData = async (provider, name, email, hashPassword, picture) => {
-  const [existingEmail] = await db.query(
-    "INSERT INTO user(provider, name, email, password, picture) VALUES (?,?,?,?,?) ;",
-    [provider, name, email, hashPassword, picture]
-  );
-  return existingEmail;
-};
+// const insertUserData = async (provider, name, email, hashPassword, picture) => {
+//   const [existingEmail] = await db.query(
+//     "INSERT INTO user(provider, name, email, password, picture) VALUES (?,?,?,?,?) ;",
+//     [provider, name, email, hashPassword, picture]
+//   );
+//   return existingEmail;
+// };
 
 const getUserDataByEmail = async (email) => {
   const [userData] = await db.query("SELECT * FROM user WHERE email = ?;", [
@@ -42,10 +44,95 @@ const getPendingUserData = async (id) => {
   return userData;
 };
 
+const signUp = async (provider, name, email, password, picture) => {
+  const conn = await db.getConnection();
+  try {
+    await conn.query("START TRANSACTION");
+
+    const checkEmail = await conn.query(
+      "SELECT email FROM user WHERE email = ? FOR UPDATE",
+      [email]
+    );
+    if (checkEmail[0].length > 0) {
+      await conn.query("COMMIT");
+      return { error: "email 已經被註冊過，請更換" };
+    }
+    const hashPassword = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
+
+    const result = await conn.query(
+      "INSERT INTO user(provider, name, email, password, picture) VALUES (?,?,?,?,?) ;",
+      [provider, name, email, hashPassword, picture]
+    );
+    const userId = result[0].insertId;
+    const payload = {
+      id: userId,
+      name: name,
+      email: email,
+      provider: provider,
+      picture: picture,
+    };
+    const token = await jwtTokenGenerater(payload);
+    const userResult = userinfoFormat(token, payload);
+
+    await conn.query("COMMIT");
+    return userResult;
+  } catch (error) {
+    console.log(error);
+    await conn.query("ROLLBACK");
+    return { error };
+  } finally {
+    await conn.release();
+  }
+};
+
+const nativeSignIn = async (email, password) => {
+  const conn = await db.getConnection();
+
+  try {
+    await conn.query("START TRANSACTION");
+
+    const [users] = await conn.query("SELECT * FROM user WHERE email = ? ;", [
+      email,
+    ]);
+    const user = users[0];
+    const hashPassword = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
+
+    if (user.password !== hashPassword) {
+      await conn.query("COMMIT");
+      return { error: "密碼錯誤" };
+    }
+
+    const payload = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      provider: user.provider,
+      picture: user.picture,
+    };
+
+    const token = await jwtTokenGenerater(payload);
+    const userResult = userinfoFormat(token, payload);
+    return userResult;
+  } catch (error) {
+    await conn.query("ROLLBACK");
+    return { error };
+  } finally {
+    await conn.release();
+  }
+};
+
 module.exports = {
-  checkExistedEmail,
-  insertUserData,
-  getUserDataByEmail,
+  // checkExistedEmail,
+  // insertUserData,
+  // getUserDataByEmail,
   getUserData,
   getPendingUserData,
+  signUp,
+  nativeSignIn,
 };
