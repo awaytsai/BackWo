c9318b4949b5a3dc94fba7f7dc7071e9062e652f
vinny tsai <away0102@gmail.com>
2021-11-28 15:53:45 +0800

    fix: fix breed data file structure

--- Router/breedData.js
+++ Router/breedData.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const breedsList = require("../public/data/breeds.json");
+const breedsList = require("../data/breeds.json");
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 // upload page
 router.route("/getBreeds").get((req, res) => {
-  res.json(breedsList);
+  return res.json(breedsList);
 });
 
 module.exports = router;
--- util/aws_reko.js
+++ util/aws_reko.js
@@ -1,6 +1,6 @@
 const aws = require("aws-sdk");
 const fetch = require("node-fetch");
-const breedsList = require("../public/data/breeds.json");
+const breedsList = require("../data/breeds.json");
 
 const Lables = require("../model/labels_model");
 const Notification = require("../model/notification_model");
@@ -31,53 +31,45 @@ async function awsReko(param, photo, postId, breed, person, county, date) {
   rekognition.detectLabels(params, async (err, data) => {
     if (err) console.log(err, err.stack);
     else console.log(data);
-    // 4. AI detect and store labels
+
+    // labels
     data.Labels.map((lb) => {
       labels.push(lb.Name);
     });
-    // console.log(labels);
     labels.map((lb) => {
       breedsList.dog_breed_en.map((breed) => {
-        // check breed
+        // check if label detect pet breed
         if (lb == breed) {
           detectBreed =
             breedsList.dog_breed[breedsList.dog_breed_en.indexOf(breed)];
         }
       });
     });
-    // store label data
-    const labelResult = await Lables.storeLable(JSON.stringify(labels), postId);
-    // update pet_post breed
+    await Lables.createLable(labels, postId);
+
     if (detectBreed) {
       breed = detectBreed;
-      const updatePostResult = await Pet.updatePetsPost(breed, postId);
+      await Pet.updatePetsPost(breed, postId);
     }
-    // check if owner post (breed/county/time) match
-    // person = "finder";
+
+    // check if post match
     person = switchPerson(param);
-    console.log("breed");
-    console.log(breed);
     const matchBreedData = await Pet.getMatchBreedPosts(
       person,
       breed,
       county,
       date
     );
-    console.log("match done");
     if (matchBreedData.length > 0) {
       const petPostIds = [];
       matchBreedData.map((data) => {
         petPostIds.push(data.id);
       });
-      console.log(petPostIds);
+
+      // create notification
       const notiStauts = "created";
-      // create notification data (fp_id/fo_id/)
-      const notificationData = await Notification.insertNotification(
-        petPostIds,
-        postId,
-        notiStauts
-      );
-      console.log(notificationData);
+      await Notification.insertNotification(petPostIds, postId, notiStauts);
+
       const response = { status: "updated" };
       return response;
     }
