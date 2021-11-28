ae3acba00d3a21de12fcaf80e3500a340db56584
vinny tsai <away0102@gmail.com>
2021-11-29 01:50:38 +0800

    fix: remove unnecessary code

--- db.js
+++ db.js
@@ -29,17 +29,6 @@ let mysqlEnv = mysqlConfig[env];
 
 const db = mysql.createPool(mysqlEnv);
 
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_DATABASE,
-//   port: process.env.DB_PORT,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
 db.getConnection((err) => {
   if (err) throw err;
   console.log(`mysql connected! ${mysqlEnv}`);
